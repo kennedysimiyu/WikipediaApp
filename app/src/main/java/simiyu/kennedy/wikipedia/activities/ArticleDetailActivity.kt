952824b15ece970b10f6403d8534100fba81bc19
Ken Simiyu <kensimiyu2015@gmail.com>
2018-04-30 23:03:36 +0300

    register detail activity on manifest, add detail activity functionality

--- app/src/main/java/simiyu/kennedy/wikipedia/activities/ArticleDetailActivity.kt
+++ app/src/main/java/simiyu/kennedy/wikipedia/activities/ArticleDetailActivity.kt
@@ -1,11 +1,18 @@
 package simiyu.kennedy.wikipedia.activities
 
+import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import simiyu.kennedy.wikipedia.R
 
 /**
  * Created by kensi on 30/04/2018.
  */
 
 class ArticleDetailActivity : AppCompatActivity()  {
+
+    override  fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_article_detail)
+    }
 
 }

fb16a9e2460fdaee64322b5a90596d59e0d979af
Ken Simiyu <kensimiyu2015@gmail.com>
2018-04-30 22:17:06 +0300

    details activity class file

---
+++ app/src/main/java/simiyu/kennedy/wikipedia/activities/ArticleDetailActivity.kt
@@ -0,0 +1,11 @@
+package simiyu.kennedy.wikipedia.activities
+
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * Created by kensi on 30/04/2018.
+ */
+
+class ArticleDetailActivity : AppCompatActivity()  {
+
+}

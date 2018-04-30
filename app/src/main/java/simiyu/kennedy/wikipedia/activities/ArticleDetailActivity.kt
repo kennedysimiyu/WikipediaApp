c797f17f5eaa2c49b925650e41853f54052075f2
Ken Simiyu <kensimiyu2015@gmail.com>
2018-04-30 23:33:07 +0300

    handling selcted items

--- app/src/main/java/simiyu/kennedy/wikipedia/activities/ArticleDetailActivity.kt
+++ app/src/main/java/simiyu/kennedy/wikipedia/activities/ArticleDetailActivity.kt
@@ -2,6 +2,8 @@ package simiyu.kennedy.wikipedia.activities
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.MenuItem
+import kotlinx.android.synthetic.main.activity_article_detail.*
 import simiyu.kennedy.wikipedia.R
 
 /**
@@ -13,6 +15,17 @@ class ArticleDetailActivity : AppCompatActivity()  {
     override  fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_article_detail)
+
+        /* set the support action bar and back button */
+        setSupportActionBar(toolbar);
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true);
     }
 
+    /* handling the selected item */
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        if(item!!.itemId == android.R.id.home){
+            finish()
+        }
+        return true;
+    }
 }
